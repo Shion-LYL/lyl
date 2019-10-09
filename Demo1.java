8ecf42767556fe3dc38f1d73cfcb261318321f12
Shion-LYL <56341580+Shion-LYL@users.noreply.github.com>
2019-10-09 16:54:35 +0800

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,14 @@
+package day02;
+
+/**
+常量  固定不变的量
+ */
+public class Demo1 {
+    //程序的主入口 main方法   psvm
+    public static void main(String[] args) {
+        System.out.println(-3);   //整数类型常量
+        System.out.println(2.3457); // 小数类型 双精度double类型
+        System.out.println('男');    //字符 常量
+        System.out.println("我在学习Java编程语言！");  //字符串 常量
+    }
+}
